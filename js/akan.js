9466b61cb09b1834a6596b214734daa08817eda9
BrianKhaifah <briankhaifah@gmail.com>
2021-06-11 15:05:39 +0300

    added a function on js with let variable

---
+++ js/akan.js
@@ -0,0 +1,12 @@
+function akan(){
+    function akanFunction(){
+        let year = document.getElementById("year").value;
+        let month = document.getElementById("month").value;
+        let date = document.getElementById("date").value;
+        let gender = document.getElementById("gender").value;
+
+        let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+        let maleNames = ["Kwase","Kwadwo","Kwabena","Kwaku","Yaw","Kofe","Kwame"]
+        let femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+    }
+}
